62501b11ecdc2ed26bc12b81bcf4eee5944358a9
abha24 <77336986+abha24@users.noreply.github.com>
2021-05-24 19:37:58 +0530

    Update sketch1.js

--- sketch1.js
+++ sketch1.js
@@ -49,14 +49,7 @@ jerry.addAnimation("jerryTeasing", jerryImg2);
  jerry.frameDelay = 25; 
 jerry.changeAnimation("jerryTeasing"); } 
   
-{ if(keyCode === LEFT_ARROW){
- tom.velocityX = -5; 
-tom.addAnimation("tomRunning", tomImg2); 
-tom.changeAnimation("tomRunning"); 
-jerry.addAnimation("jerryTeasing", jerryImg2); 
-jerry.frameDelay = 25; 
-jerry.changeAnimation("jerryTeasing"); } }
-    
+
  drawSprites();
     
   
